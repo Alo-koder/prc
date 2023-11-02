262698d40d356b338aa6fb78b9590c09556b8711
Aleksander Szewczyk <alek.szewczyk.as@gmail.com>
2023-11-02 11:23:36 +0100

    More precise period determination.
    Now it's based on zero-crossings instead of I peaks.

--- phase.py
+++ phase.py
@@ -3,16 +3,32 @@ from scipy.signal import find_peaks
 from matplotlib import pyplot as plt
 import numpy as np
 
-df, pert = read_data('T:\\Team\\Szewczyk\\Data\\20231024\\raw\\A00102.dat',
-                     margins=(1500, 2500))
+def find_periods(df):
+    
+    mean_current = df.mean()['I']
+    df['I relative'] = df['I']-mean_current
+    crossings = df[(np.diff(np.sign(df['I relative']), append=0) > 0)]
+    periods = np.diff(crossings['t'])
+    
+    return periods, crossings['t'][:-1]
 
-peak_indicies, _ = find_peaks(df['EMSI'], distance=100, prominence=0.1)
-peaks = df.iloc[peak_indicies]
+# =============================================================================
+# plt.plot(df['t'], df['I'])
+# plt.hlines(mean_current, 100, 5000, colors='r')
+# plt.scatter(crossings['t'], crossings['I'], c='g', marker='x')
+# plt.xlim(2400, 2500)
+# plt.show() 
+# =============================================================================
 
-plt.plot(df['t'], df['EMSI'])
-plt.scatter(peaks['t'], peaks['EMSI'], marker='x', c='r')
-plt.vlines(pert['t'], 15.7, 16.6, linestyle='dashed', colors='y')
-plt.show()
-
-peak_times = np.array(peaks['t'])
-periods = (peak_times-np.roll(peak_times, 1))[1:]
+if __name__ == '__main__':
+    df, pert = read_data('T:\\Team\\Szewczyk\\Data\\20231024\\raw\\A00102.dat',
+                         margins=(1800, 3400))
+    periods, crossings = find_periods(df)
+    
+    plt.scatter(crossings, periods, marker='+')
+    plt.plot(crossings, periods)
+    plt.vlines(pert['t'], 40, 50, colors='r', linestyles='dashed')
+    
+    plt.ylim(40, 50)
+    plt.show()
+    
