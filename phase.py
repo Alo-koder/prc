59f4865c7d06d8af41c11d15e323f6599040860a
Aleksander Szewczyk <alek.szewczyk.as@gmail.com>
2023-11-08 16:35:09 +0100

    Potentiostat data
    1. Switched from EMSI to potentiostat current data.
    2. Added pert. phase determination.
    3. First attempt at PRC. Invalid method used, no meaningful data.

--- phase.py
+++ phase.py
@@ -1,5 +1,6 @@
-from read_data import read_data
-from scipy.signal import find_peaks
+#from read_data import read_data
+from read_VA import read_VA
+#from scipy.signal import find_peaks
 from matplotlib import pyplot as plt
 import numpy as np
 
@@ -10,7 +11,7 @@ def find_periods(df):
     crossings = df[(np.diff(np.sign(df['I relative']), append=-1) > 0)]
     periods = np.diff(crossings['t'])
     
-    return periods, crossings['t'][:-1]
+    return periods, np.array(crossings['t'][:-1])
 
 # =============================================================================
 # plt.plot(df['t'], df['I'])
@@ -20,23 +21,40 @@ def find_periods(df):
 # plt.show() 
 # =============================================================================
 
+def pert_response(crossings, mean_period, pert, periods):
+    indicies = np.searchsorted(crossings, np.array(pert))-1
+    phase = (pert-crossings[indicies])/mean_period
+    
+    # This is not valid!!! we need to wait until the line stabilises
+    response = periods[indicies+1]-mean_period
+    # This is not valid!
+    return phase, response
+
 if __name__ == '__main__':
-    df, pert = read_data('T:\\Team\\Szewczyk\\Data\\20231024\\raw\\A00102.dat',
-                         margins=(2550, 3700), p_height=1)
+    df, pert = read_VA('T:\\Team\\Szewczyk\\Data\\20231103\\A00202_C01.txt',
+                         margins=(400, 4100), p_height=0.1)
     periods, crossings = find_periods(df)
+    
+    phase, response = pert_response(crossings, np.average(periods), pert, periods)
+    plt.scatter(phase, response)
+    plt.title("INVALID FIT!!! DON'T FORGET")
+    plt.show()
     
     plt.scatter(crossings, periods, marker='+')
     plt.plot(crossings, periods)
-    plt.vlines(pert['t'], 40, 50, colors='r', linestyles='dashed')
+    plt.vlines(pert, 40, 50, colors='r', linestyles='dashed')
     
-    plt.ylim(43, 47)
+    plt.xlim(1400, 1800)
+    plt.ylim(40, 43)
     plt.title('Perturbation: +1V, 0.1s')
     plt.xlabel('Time [s]')
     plt.ylabel('Osc. period [s]')
     plt.show()
     
+    plt.xlim(500, 700)
     plt.plot(df['t'], df['I'])
     plt.xlabel('Time [s]')
     plt.ylabel('Current [A]')
-    plt.vlines(pert['t'], 3e-6, 10e-6, colors='r', linestyles='dashed')
+    plt.vlines(pert, 0.02, 0.14, colors='r')
+    plt.vlines(crossings, 0.02, 0.14, colors='g', linestyles='dashdot')
     plt.show()
--- read_VA.py
+++ read_VA.py
@@ -3,28 +3,32 @@ from scipy.ndimage import gaussian_filter
 from scipy.signal import find_peaks
 import numpy as np
 
-def read_data(filename, p_height=0.5, margins=(100, 5000)):
+def read_VA(filename, p_height=0.1, margins=(100, 5000)):
     
-    df = pd.read_csv(filename, sep='\t', header=5)
+    df = pd.read_csv(filename, sep='\t')#.astype(float)
     
     
     # Renaming columns
-    new_cols = ['t', 'U', 'I', 'Ill', 'EMSI']
+    new_cols = ['t', 'U', 'I']
     old_cols = df.columns
     col_dict = {x: y for x, y in zip(old_cols, new_cols)}
     df = df.rename(columns = col_dict)
     
-    # Flipping signs
-    df['U'] = -df['U']
-    df['I'] = -df['I']
+# =============================================================================
+#     # Flipping signs
+#     df['U'] = -df['U']
+#     df['I'] = -df['I']
+# =============================================================================
     
     # Applying margins
     df = df[(df['t'] > margins[0]) & (df['t'] < margins[1])]
     
-    # Signal smoothening
-    df['I'] = gaussian_filter(df['I'], 5)
-    df['EMSI'] = gaussian_filter(df['EMSI'], 2)
-    # df['U'] = gaussian_filter(df['U'], 1)
+# =============================================================================
+#     # Signal smoothening
+#     df['I'] = gaussian_filter(df['I'], 5)
+#     df['EMSI'] = gaussian_filter(df['EMSI'], 2)
+#     # df['U'] = gaussian_filter(df['U'], 1)
+# =============================================================================
     
     # Finding voltage spikes
     spike_indicies, _ = find_peaks(np.sign(p_height) * df['U'],
@@ -33,8 +37,9 @@ def read_data(filename, p_height=0.5, margins=(100, 5000)):
     
     return df, np.array(spikes['t'])
 
+
 if __name__ == '__main__':
-    df, spikes = read_data('T:\\Team\\Szewczyk\\Data\\20231024\\raw\\A00102.dat')
+    df, spikes = read_VA('T:\\Team\\Szewczyk\\Data\\20231103\\A00202_C01.txt')
     
     from matplotlib import pyplot as plt
     
@@ -43,5 +48,4 @@ if __name__ == '__main__':
     plt.plot(df['t'], df['U'])
     plt.scatter(spikes['t'], spikes['U'], marker='x', c='r')
     plt.show()
-    
     
