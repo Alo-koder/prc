75edca835d704e1e29aadeff8ccb73ee73438bcf
Aleksander Szewczyk <alek.szewczyk.as@gmail.com>
2023-11-10 15:41:32 +0100

    Nichts neues

--- phase.py
+++ phase.py
@@ -9,9 +9,11 @@ def find_periods(df):
     mean_current = df.mean()['I']
     df['I relative'] = df['I']-mean_current
     crossings = df[(np.diff(np.sign(df['I relative']), append=-1) > 0)]
-    periods = np.diff(crossings['t'])
     
-    return periods, np.array(crossings['t'][:-1])
+    crossings = np.array(crossings['t'])
+    periods = np.diff(crossings)
+    
+    return periods, np.array(crossings[:-1])
 
 # =============================================================================
 # plt.plot(df['t'], df['I'])
@@ -25,36 +27,41 @@ def pert_response(crossings, mean_period, pert, periods):
     indicies = np.searchsorted(crossings, np.array(pert))-1
     phase = (pert-crossings[indicies])/mean_period
     
-    # This is not valid!!! we need to wait until the line stabilises
-    response = periods[indicies+1]-mean_period
-    # This is not valid!
-    return phase, response
+    
+    xd = np.array([periods[x:x+4] for x in indicies[:-1]])
+    
+    response = np.sum(xd, axis=1)-4*mean_period
+    
+    return phase[:-1], response
 
 if __name__ == '__main__':
     df, pert = read_VA('T:\\Team\\Szewczyk\\Data\\20231103\\A00202_C01.txt',
-                         margins=(400, 4100), p_height=0.1)
+                         margins=(300, 4100), p_height=0.1)
     periods, crossings = find_periods(df)
     
     phase, response = pert_response(crossings, np.average(periods), pert, periods)
     plt.scatter(phase, response)
-    plt.title("INVALID FIT!!! DON'T FORGET")
+    plt.title("INVALID FIT!!! DON'T USE")
     plt.show()
     
-    plt.scatter(crossings, periods, marker='+')
-    plt.plot(crossings, periods)
-    plt.vlines(pert, 40, 50, colors='r', linestyles='dashed')
+# =============================================================================
+#     plt.scatter(crossings, periods, marker='+')
+#     plt.plot(crossings, periods)
+#     plt.vlines(pert, 40, 50, colors='r', linestyles='dashed')
+#     
+#     plt.xlim(2600, 2900)
+#     plt.ylim(41, 42)
+#     plt.title('Perturbation: +1V, 0.1s')
+#     plt.xlabel('Time [s]')
+#     plt.ylabel('Osc. period [s]')
+#     plt.show()
+# =============================================================================
     
-    plt.xlim(1400, 1800)
-    plt.ylim(40, 43)
-    plt.title('Perturbation: +1V, 0.1s')
-    plt.xlabel('Time [s]')
-    plt.ylabel('Osc. period [s]')
-    plt.show()
-    
-    plt.xlim(500, 700)
+    plt.xlim(700, 1000)
+    plt.hlines(df.mean()['I'], 1400, 1800, colors='y', linestyles='dashed')
     plt.plot(df['t'], df['I'])
     plt.xlabel('Time [s]')
     plt.ylabel('Current [A]')
-    plt.vlines(pert, 0.02, 0.14, colors='r')
+    plt.scatter(pert, df[df['t'].isin(pert)]['I'], marker='x', c='r')
     plt.vlines(crossings, 0.02, 0.14, colors='g', linestyles='dashdot')
     plt.show()
