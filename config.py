842f8166ae4238ab18c7a731e5aec04b71a39cdd
Aleksander Szewczyk <alek.szewczyk.as@gmail.com>
2024-04-24 13:58:51 +0200

    period meas. comparison done

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import munch
 import yaml
 
 DEVICE = 'tum'
-DATE = '03-20'
+DATE = '10-27'
 
 if DEVICE == 'laptop':
     filenames_dict = {
@@ -12,7 +12,7 @@ if DEVICE == 'laptop':
 
 elif DEVICE == 'tum':
     filenames_dict = {
-        'data' : 'T:/Team/Szewczyk/Data/2024-'+DATE+'/',
+        'data' : 'T:/Team/Szewczyk/Data/2023-'+DATE+'/',
         'notes' : 'C:/Users/go68nim/OneDrive - University of Edinburgh/Notes/Lab/experiments/plots/'+DATE[1:]+'/'
     }
 
